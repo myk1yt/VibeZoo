accbf81f1e45b3651192b3c773c09dd2e094fd86
k1yt <myk1yt@gmail.com>
2026-06-02 15:53:05 +0900

    chore: cleanup temporary fix and test scripts

--- _check_mmproj2.py
+++
@@ -1,9 +0,0 @@
-import sys  
-sys.path.insert(0, 'mcp-servers')  
-from llama_cpp import Llama  
-from bridge.vision.minicpm import _get_model_path, _get_mmproj_path  
-mp = str(_get_mmproj_path())  
-import os  
-print('mmproj path:', mp)  
-print('mmproj exists:', os.path.exists(mp))  
-print('mmproj size:', os.path.getsize(mp)/1024/1024, 'MB')  
